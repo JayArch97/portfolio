64235f2635e8697b7cd1338612ef491ad631f87c
Jayarch97 <jayquintanilla2@gmail.com>
2025-07-01 01:52:43 +0000

    Adding base of the navbar

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -7,6 +7,8 @@ import { TextureLoader } from "three";
 import sphereTexture from "./assets/4k_haumea_fictional.jpg";
 import homePageBackground from "./assets/8k_stars_milky_way.jpg";
 import linkedIn from './assets/linkedin.png'
+import meduim from './assets/medium.png'
+import NavBar from './NavBar.tsx'
 
 function MyRotatingBox(props) {
   const colorMap = useLoader(TextureLoader, sphereTexture);
@@ -39,12 +41,14 @@ function MyRotatingBox(props) {
 export default function HomePage() {
   return (
     <>
+ 
       <div
+      
         style={{
           backgroundImage: `url(${homePageBackground})`,
           backgroundSize: "cover",
-        }}
-      >
+        }}>
+             <NavBar/>
         <div className="container mx-auto p-4 ">
           <h1 className="text-4xl font-mono">Julio Quintanilla</h1>
           <p className="mt-2">Check out some of my projects:</p>
@@ -68,18 +72,18 @@ export default function HomePage() {
           <img
             src={profilepic}
             alt="Profile"
-            className="w-full/2 h-[60vh] rounded-md mx-auto mt-4  shadow-[inset_0_-2px_0px] shadow-cyan-500/50 opacity-85 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer "
+            className="w-full/2 h-[60vh] rounded-md mx-auto mt-4  shadow-xl shadow-cyan-500/50 mask-b-from-90% mask-b-to-95% shadow-cyan-500/50 opacity-85 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer "
           />
           <div className="flex items-center justify-center ">
           <Link
-            to="https://github.com/JayArch97"
+            to="https://www.linkedin.com/in/julio-armando-quintanilla-acosta-bb0582318/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
               src={linkedIn}
               alt="Github"
-              className="w-10 h-10 mt-10 rounded-full mx-auto object-contain shadow-xl/30 shadow-cyan-500/50 opacity-85 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
+              className="w-10 h-10 mt-10 rounded-full mx-auto object-contain  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
             />
             <div className="text-center  px-2 py-2 mx-auto">
               <p className="text-lg">LinkedIn</p>
@@ -93,10 +97,24 @@ export default function HomePage() {
             <img
               src={github}
               alt="Github"
-              className="w-10 h-10 mt-10 rounded-full mx-auto object-contain shadow-xl/30 shadow-cyan-500/50 opacity-85 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
+              className="w-10 h-10 mt-10 rounded-full mx-auto object-contain  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
             />
             <div className="text-center px-2 py-2 mx-auto">
               <p className="text-lg">GitHub</p>
+            </div>
+          </Link>
+          <Link
+            to="https://medium.com/@jayarch"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={meduim}
+              alt="Github"
+              className="w-10 h-10 mt-10 rounded-full mx-auto object-contain shadow-xl/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
+            />
+            <div className="text-center px-2 py-2 mx-auto">
+              <p className="text-lg">Meduim</p>
             </div>
           </Link>
         </div>
---
+++ src/NavBar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+
+export default function NavBar(){
+
+    return(
+        <nav className="flex justify-center space-x-5">
+            <ul className="flex space-x-5 hover:cursor-pointer">
+                <Link to = '/home'>
+                <li>Home</li>
+                </Link>
+                <Link to = '/about'>
+                <li>About</li>
+                </Link>
+                <Link to = '/contact'>
+                <li>Contact</li>
+                </Link>
+            </ul>
+        </nav>
+    )
+}
+
+
